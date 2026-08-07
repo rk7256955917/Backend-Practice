f2ca2349e9f9364714d4cfad6ea46cd2339b74f2
Rohit Kumar <147175132+rk7256955917@users.noreply.github.com>
2026-08-08 01:11:14 +0530

    image upload

--- project6/app.js
+++ project6/app.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
+const multer = require("multer");
 app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
-    res.render("index");
-})
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"uploads/");
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname);
+    }
+});
+
+const upload = multer({storage:storage});
+app.use(express.static("uploads"));
+
+// app.get("/",(req,res)=>{
+//     res.render("index");
+// })
+
+app.post("/upload",upload.single("image"),(req,res)=>{
+    console.log(req.file);
+    
+    res.render("index",{
+        image:req.file.filename
+    });
+});
 
 app.listen(3000,()=>{
     console.log(`server is running on 3000`);
